ab22ae40f72f09c4d8c943c25eb295177ac6644e
petramello <petramello@gmail.com>
2021-06-09 12:08:20 -0300

    App.js: filttering users and creating UserDetail component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+
+
+const UserDetail = ({ user, onBack }) => {
+  return (
+    <div className="Container">
+      {Object.keys(user).map(attribute => (
+        <div>
+          {typeof user[attribute] === "string"
+            ? `${attribute}: ${user[attribute]}`
+            : null}
+          <br/>
+        </div>
+      ))}
+      <br/>
+      <button className="BackButton" onClick={onBack}>Back</button>
+    </div>
+  )
+}
+
+
 function App() {
 
+  const [ selectedUser, setSelectedUser ] = useState(null);
   const [users, setUsers] = useState([]);
+  const [ searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const promise = axios.get('https://jsonplaceholder.typicode.com/users');
@@ -14,15 +36,32 @@ function App() {
     })
   }, []);
 
-  console.log(users);
-
-  return users.map( ({ name, email }) => {
+  const userList =  users
+    .filter( ({ name }) => name.includes(searchTerm))
+    .map( user => {
     return (
-      <div>
-        NAME: { name }, EMAIL:  { email }
+      <div  key={user.id}>
+        <br />
+        <span onClick={() => setSelectedUser(user)} style={{cursor: "pointer"}} >
+          NAME: { user.name }, EMAIL:  { user.email }
+        </span>
       </div>
     )
-  })
+  });
+
+
+  if(selectedUser) {
+    return (
+      <UserDetail user={selectedUser} onBack={() => setSelectedUser(null)} />
+    )
+  }
+
+  return (
+    <div className="Container">
+      <input className="AppInput" onChange={event => setSearchTerm(event.target.value)} type="text" placeholder="Type user name to filter" />
+        { userList }
+    </div>
+  )
 }
 
 export default App;
