2ae058495d0257c9ea7d37d6f36b2a4b160b7d1e
petramello <petramello@gmail.com>
2021-06-09 10:08:08 -0300

    App.js: achieving the promise; recieving data from promise; and setting State users to data.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
-
-
 function App() {
 
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-  }, [])
+    const promise = axios.get('https://jsonplaceholder.typicode.com/users');
+    promise.then(response => {
+      // const data = response.data --- this code is the same bellow, which is destructured
+      const { data } = response;
+      setUsers(data);
+    })
+  }, []);
 
-
+  console.log(users);
+  
   return (
     <div className="App">
       <h1>Hello!</h1>
